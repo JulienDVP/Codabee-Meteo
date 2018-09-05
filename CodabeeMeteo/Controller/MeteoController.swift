83126ec895710b5b55ebf42251fac0b24ab298ff
Julien <julien@MacBook-Pro-de-Julien.local>
2018-09-05 16:09:38 +0200

    Premier commit

---
+++ CodabeeMeteo/Controller/MeteoController.swift
@@ -0,0 +1,35 @@
+//
+//  MeteoController.swift
+//  CodabeeMeteo
+//
+//  Created by Julien on 05/09/2018.
+//  Copyright © 2018 Julien. All rights reserved.
+//
+
+import UIKit
+
+class MeteoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CodabeeMeteo/Ressources/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  CodabeeMeteo
+//
+//  Created by Julien on 05/09/2018.
+//  Copyright © 2018 Julien. All rights reserved.
+//
+
+import Foundation
